ff59a17ab8dabe8f56d0aec4abea0e14cf6790bb
J.Atkins <89127977+End-Us3r@users.noreply.github.com>
2024-07-19 20:29:37 -0700

    Update to components

---
+++ jammming/src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Tracklist () {
+    return (
+        <dvi className='Tracklist'>
+            {/* You will add a map method that renders a set of Track compontents */}
+        </dvi>
+    );
+}
+
+export default Tracklist;
